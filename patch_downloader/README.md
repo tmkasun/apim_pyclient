00edd79f77db2a296634c136be447ec6f3bcbc28
Kasun Thennakoon <tmkasun@gmail.com>
2018-06-21 10:59:52 +0530

    Support full patch name

---
+++ patch_downloader/README.md
@@ -0,0 +1,10 @@
+# Download patches given the list of patches needs to be downloaded
+# *** THIS IS NOT A TOOL TO ILLEGALLY DOWNLOAD WSO2 PATCHES/UPDATES ***
+ - To use this tool you need to have a wso2 OT account
+ - To update the product with updates please use the [wso2 WUM tool](https://wso2.com/updates/wum)
+ - `patch.list` support both
+   - WSO2-CARBON-PATCH-4.4.0-xxxx.zip
+   - patchxxxx
+ - file formats
+
+ 
--- patch_downloader/downloader.py
+++ patch_downloader/downloader.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import requests
 import base64
 from subprocess import call
 import time
+import getpass
 
 name_matrix = {
     "4.2.0": "turing",
@@ -17,22 +19,31 @@ base_url = "https://svn.wso2.com/wso2/custom/projects/projects/carbon/{kernel_na
 
 
 def download(patch_number, session):
-    kernel_name = name_matrix[configs["kernel"]]
-    patch_name = "WSO2-CARBON-PATCH-{kernel_version}-{patch_number}".format(kernel_version=configs["kernel"],
-                                                                            patch_number=patch_number)
+    if "WSO2-CARBON-PATCH" in patch_number:
+        patch_name = patch_number
+        patch_naming_segments = patch_name.split('-')
+        patch_number = patch_naming_segments[-1]
+        configs["kernel"] = patch_naming_segments[-2]
+        kernel_name = name_matrix[configs["kernel"]]
+    else:
+        kernel_name = name_matrix[configs["kernel"]]
+        patch_name = "WSO2-CARBON-PATCH-{kernel_version}-{patch_number}".format(kernel_version=configs["kernel"],
+                                                                                patch_number=patch_number)
     file_name = "{patch_name}.zip".format(patch_name=patch_name)
     request_url = base_url.format(kernel_name=kernel_name, patch_number=patch_number, kernel_version=configs["kernel"],
                                   file_name=file_name)
+    print("Downloading {}".format(request_url))
     response = session.get(request_url)
     if response.ok:
-        print("Downloading {}".format(file_name))
         with open('./downloads/{file_name}'.format(file_name=file_name), 'wb') as f:
             f.write(response.content)
         print("Unziping {}".format(file_name))
         call(["unzip", "./downloads/{}".format(file_name), "-d", "./downloads/"])
-        call(["cp", "-R", "./downloads/{}/patch{}".format(patch_name, patch_number), "./downloads/patches/"])
+        call(["cp", "-R", "./downloads/{}/patch{}".format(patch_name,
+                                                          patch_number), "./downloads/patches/"])
     else:
-        print("*** ERROR Downloading {}".format(file_name))
+        print("*** ERROR Downloading {} \n Reason: {} <{}>".format(file_name, response.reason, response.status_code))
+
 
 def connection():
     session = requests.Session()
@@ -40,12 +51,16 @@ def connection():
         "{}:{}".format(configs["username"], configs["password"]).encode()).decode()
     return session
 
+
 def main():
     if "username" not in configs:
         configs["username"] = input("Enter WSO2 OT username: ")
-    configs["password"] = input("Enter password for {}: ".format(configs['username']))
-    kernel = input("Enter Carbon Kernel version number (Default 4.4.0): ")
-    patch_list = input("Enter path to patch list file (Default ./patch.list): ")
+    configs["password"] = getpass.getpass(
+        "Enter password for {}: ".format(configs['username']))
+    kernel = input(
+        "Enter Carbon Kernel version number (Ignore if given the full patch name) (Default 4.4.0): ")
+    patch_list = input(
+        "Enter path to patch list file (Default ./patch.list): ")
     if kernel:
         configs["kernel"] = kernel
     if patch_list:
